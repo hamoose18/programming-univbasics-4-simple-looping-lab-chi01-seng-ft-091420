eb04542f1fdee8a1d9a80543a541f5a159cacd06
arhamm ghazali <arhamm_ghazali93@hotmail.com>
2020-09-08 18:00:15 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,15 @@
-# Write your methods here
+def loop_message_five_times(Hello_World.)
+  count = 0
+  while count < 5 do 
+    puts "Hello World."
+    count += 1 
+  end
+end
+
+def loop_message_n_times
+  count = 0 
+  while count < 5 do 
+    puts "hello Moon."
+    count += l
+  end 
+end
