9fb201b8015237309d5ad55f966d1fc2b5939b91
arhamm ghazali <arhamm_ghazali93@hotmail.com>
2020-09-08 18:26:34 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,15 +1,7 @@
-def loop_message_five_times(Hello_World.)
+def loop_message_five_times(message)
   count = 0
-  while count < 5 do 
-    puts "Hello World."
-    count += 1 
+  while count < 5 do
+    puts message
+    count += 1
   end
-end
-
-def loop_message_n_times
-  count = 0 
-  while count < 5 do 
-    puts "hello Moon."
-    count += l
-  end 
 end
